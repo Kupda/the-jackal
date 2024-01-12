7a4a95fcf3ba32494648450a05d2c8f2549455be
Kupda <kupda27@gmail.com>
2024-01-12 21:02:04 +0300

    Добавил экраны меню и настроек

---
+++ screens/menu_screen.py
@@ -0,0 +1,50 @@
+import pygame
+import button
+import time
+import os
+from .events import OPEN_SETTINGS_SCREEN, OPEN_GAME_SCREEN
+
+
+resume_img = pygame.image.load("images/start_game.png")
+options_img = pygame.image.load("images/options.png")
+quit_img = pygame.image.load("images/quit.png")
+replay_img = pygame.image.load("images/replay.png")
+video_img = pygame.image.load('images/button_video.png')
+audio_img = pygame.image.load('images/button_audio.png')
+keys_img = pygame.image.load('images/button_keys.png')
+back_img = pygame.image.load('images/button_back.png')
+TEXT_COL = (255, 255, 255)
+
+
+class MenuScreen():
+    def __init__(self, screen, font):
+        self.x, self.y = screen.get_size()
+        self.font = font
+        self.screen = screen
+        self.resume_button = button.Button(self.x / 2 - 150, self.y / 2 - 150, resume_img, 0.75, self.start_game)
+        self.replay_button = button.Button(self.x / 2 - 150, self.y / 2 + 0, replay_img, 0.75)
+        self.options_button = button.Button(self.x / 2 - 150, self.y / 2 + 150, options_img, 0.75, self.open_settings)
+        self.quit_button = button.Button(self.x / 2 - 150, self.y / 2 + 300, quit_img, 0.75, self.quit_game)
+
+    def draw_text(self, text, text_col, x, y):
+        img = self.font.render(text, True, text_col)
+        self.screen.blit(img, (x, y))
+
+    def draw(self):
+        self.draw_text("GAME", TEXT_COL, self.x / 2 - 50, self.y / 2 - 300)
+        self.resume_button.draw(self.screen)
+        self.replay_button.draw(self.screen)
+        self.options_button.draw(self.screen)
+        self.quit_button.draw(self.screen)
+
+    def quit_game(self):
+        quit_event = pygame.event.Event(pygame.QUIT)
+        pygame.event.post(quit_event)
+
+    def open_settings(self):
+        open_settings_event = pygame.event.Event(OPEN_SETTINGS_SCREEN)
+        pygame.event.post(open_settings_event)
+
+    def start_game(self):
+        start_game_event = pygame.event.Event(OPEN_GAME_SCREEN)
+        pygame.event.post(start_game_event)
---
+++ screens/settings_screen.py
@@ -0,0 +1,55 @@
+import pygame
+import button
+import time
+import os
+from .events import OPEN_MENU_SCREEN
+from audio import audio
+
+
+back_img = pygame.image.load('images/quit.png')
+sound_on_img = pygame.image.load('images/on_sound.png')
+sound_off_img = pygame.image.load('images/off_sound.png')
+music_on_img = pygame.image.load('images/on_music.png')
+music_off_img = pygame.image.load('images/off_music.png')
+TEXT_COL = (255, 255, 255)
+
+
+class SettingsScreen():
+    def __init__(self, screen, font):
+        self.x, self.y = screen.get_size()
+        self.font = font
+        self.screen = screen
+        self.back_button = button.Button(100, 50, back_img, 0.75, self.back_to_menu)
+
+        self.sound_on_btn = button.Button(self.x / 2 - 150, self.y / 2 - 150, sound_on_img, 0.75, self.toggle_sound)
+        self.sound_off_btn = button.Button(self.x / 2 - 150, self.y / 2 - 150, sound_off_img, 0.75, self.toggle_sound)
+        self.music_on_btn = button.Button(self.x / 2 - 150, self.y / 2 + 0, music_on_img, 0.75, self.toggle_music)
+        self.music_off_btn = button.Button(self.x / 2 - 150, self.y / 2 + 0, music_off_img, 0.75, self.toggle_music)
+
+    def draw_text(self, text, text_col, x, y):
+        img = self.font.render(text, True, text_col)
+        self.screen.blit(img, (x, y))
+
+    def draw(self):
+        self.draw_text("Settings", TEXT_COL, self.x / 2 - 50, self.y / 2 - 300)
+        self.back_button.draw(self.screen)
+        if audio.sounds_muted:
+            self.sound_on_btn.draw(self.screen)
+        else:
+            self.sound_off_btn.draw(self.screen)
+
+        if audio.music_muted:
+            self.music_on_btn.draw(self.screen)
+        else:
+            self.music_off_btn.draw(self.screen)
+
+    def back_to_menu(self):
+        back_event = pygame.event.Event(OPEN_MENU_SCREEN)
+        pygame.event.post(back_event)
+
+    def toggle_sound(self):
+        audio.toggle_sounds()
+
+    def toggle_music(self):
+        audio.toggle_music()
+
